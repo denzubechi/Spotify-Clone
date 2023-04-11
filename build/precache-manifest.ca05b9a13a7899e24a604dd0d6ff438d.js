20cb37fa658c1c4a10bf2a5312e54bfde3a8243b
denzubechi <chukwumasamuel371@gmail.com>
2023-04-11 00:43:29 -0700

    feat:added build

---
+++ build/precache-manifest.ca05b9a13a7899e24a604dd0d6ff438d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166e8a30d32d99dd1b3e1ca82de65a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "924187710725c531678f",
+    "url": "/static/css/main.df2ad7c8.chunk.css"
+  },
+  {
+    "revision": "f4712fc41f1db93641d7",
+    "url": "/static/js/2.b36705d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b36705d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924187710725c531678f",
+    "url": "/static/js/main.752e1bb6.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
